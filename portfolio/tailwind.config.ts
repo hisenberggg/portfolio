18a0b9d43410c2a5b55a20fdf686d3632f4d3124
Abhijeet Ringe <ringeabhijeet9@gmail.com>
2024-07-10 02:04:30 +0530

    updated tailwind

--- portfolio/tailwind.config.ts
+++ portfolio/tailwind.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      typography: (theme) => ({
+      typography: (theme: (arg0: string) => any) => ({
         white: {
           css: {
             color: theme('colors.white'),
